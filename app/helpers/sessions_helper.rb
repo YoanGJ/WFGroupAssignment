6daf8ffc2f2ff8d3f824c222408f342530d42f73
Yoan Grandjacques <yoan.grandjacques@icloud.com>
2017-04-23 15:30:52 +0000

    login admin

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,30 @@
+module SessionsHelper
+    
+    def log_in_admin(admin)
+        session[:admin_id] = admin.id
+    end
+    
+    def current_admin
+       @current_admin ||= Admin.find_by(id: session[:admin_id])
+    end
+    
+    def admin_logged_in?
+       !current_admin.nil?
+    end
+    
+    def remember_admin(admin)
+       admin.remember_admin
+       cookies.permanent.signed[:admin_id] = admin.id
+       cookies.permanent[:remember_token] = admin.remember_token
+    end
+    
+    def log_out_admin
+       session.delete(:admin_id)
+       @current_admin = nil
+    end
+    
+    def destroy
+       log_out_admin
+       redirect_to root_url 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   resources :school_modules
   resources :students
   
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout'    => 'sessions#destroy'
+  get 'admin' => 'admin#index'
 end
