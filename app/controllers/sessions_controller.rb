debd7de33f2fe3b2fc5b6097663882350005b829
Yoan Grandjacques <yoan.grandjacques@icloud.com>
2017-04-25 13:29:34 +0000

    students can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,17 @@ class SessionsController < ApplicationController
   
   def create
     admin = Admin.find_by(email: params[:session][:email].downcase)
+    student = Student.find_by(email: params[:session][:email].downcase)
     if admin && admin.authenticate(params[:session][:password])
       log_in_admin admin
       redirect_to admin
+    elsif student && student.authenticate(params[:session][:password])
+      log_in_student student
+      redirect_to student
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,12 +4,24 @@ module SessionsHelper
         session[:admin_id] = admin.id
     end
     
+    def log_in_student(student)
+        session[:student_id] = student.id
+    end
+    
     def current_admin
        @current_admin ||= Admin.find_by(id: session[:admin_id])
     end
     
+    def current_student
+       @current_student ||= Student.find_by(id: session[:student_id])
+    end
+    
     def admin_logged_in?
        !current_admin.nil?
+    end
+    
+    def student_logged_in?
+       !current_student.nil? 
     end
     
     def remember_admin(admin)
@@ -18,13 +30,30 @@ module SessionsHelper
        cookies.permanent[:remember_token] = admin.remember_token
     end
     
+    def remember_student(student)
+       student.remember_student
+       cookies.permanent.signed[:student_id] = student.id
+       cookies.permanent[:remember_token] = student.remember_token
+    end
+    
     def log_out_admin
        session.delete(:admin_id)
        @current_admin = nil
+    end
+    
+    def log_out_student
+       session.delete(:student_admin)
+       @current_student = nil
     end
     
     def destroy
        log_out_admin
        redirect_to root_url 
     end
+    
+    def destroy
+        log_out_student
+        redirect_to root_url
+    end
+    
 end
